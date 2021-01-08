2a65680a68f9faa80a4fc55230cb58a64623c7ef
RealActimel <Activelione@gmail.com>
2021-01-08 23:07:03 +0100

    Online frisch

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,16 +7,15 @@ public class Player : NetworkBehaviour
 {
     void HandleMovement()
     {
-        if (isLocalPlayer)
-        {
+        if (isLocalPlayer){
             float moveHorizontal = Input.GetAxis("Horizontal");
             float moveVertical = Input.GetAxis("Vertical");
-            Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0);
+            Vector3 movement = new Vector3(moveHorizontal * 0.1f, moveVertical * 0.1f, 0);
             transform.position = transform.position + movement;
         }
     }
-
-    void update()
+    
+    void Update()
     {
         HandleMovement();
     }
