7f3a86d28c6b42ddd81bda1306b1e9fd907b7b70
RealActimel <Activelione@gmail.com>
2021-01-09 00:25:51 +0100

    Animation rot fertig

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MyNetworkManager : NetworkManager
+{
+    public override void OnStartServer()
+    {
+        Debug.Log("Server Startet!");
+    }
+
+    public override void OnStopServer()
+    {
+        Debug.Log("Server Stopped");
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+       Debug.Log("Connected to Server!");
+    }
+
+    public override void OnClientDisconnect(NetworkConnection conn)
+    {
+        Debug.Log("Disconnected from Server!");
+    }
+}
