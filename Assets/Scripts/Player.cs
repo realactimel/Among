3e5bc6ab9778dacdd7b320cf1f7699757da789b3
RealActimel <Activelione@gmail.com>
2021-01-09 16:25:13 +0100

    Lauf animation fertig

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,14 +5,38 @@ using UnityEngine;
 
 public class Player : NetworkBehaviour
 {
+    public Animator animator;
+    bool facingRight = true;
     void HandleMovement()
-    {
+    {  
         if (isLocalPlayer){
             float moveHorizontal = Input.GetAxis("Horizontal");
+
+            if(moveHorizontal<0 && facingRight)
+            {
+                flip();
+            }
+            if(moveHorizontal>0 && !facingRight)
+            {
+                flip();
+            }
+
+        animator.SetFloat("Speed", Mathf.Abs(moveHorizontal));
+
             float moveVertical = Input.GetAxis("Vertical");
+
             Vector3 movement = new Vector3(moveHorizontal * 0.1f, moveVertical * 0.1f, 0);
             transform.position = transform.position + movement;
+            animator.SetFloat("Speed", Mathf.Abs(moveVertical));
+        
         }
+        
+    }
+
+    void flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f,0f);
     }
     
     void Update()
